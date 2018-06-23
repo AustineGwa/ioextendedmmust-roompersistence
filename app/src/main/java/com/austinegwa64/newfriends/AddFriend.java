4c7728162aaa2cb64d71379146e09e06409f5794
Austine Gwa <austinegwa64@gmail.com>
2018-06-23 20:08:34 +0300

    fixed the error of non matching database names

--- app/src/main/java/com/austinegwa64/newfriends/AddFriend.java
+++ app/src/main/java/com/austinegwa64/newfriends/AddFriend.java
@@ -14,33 +14,22 @@ import com.austinegwa64.newfriends.Database.Friend;
 public class AddFriend extends AppCompatActivity {
 
     Button button;
-    AppDatabase db;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_friend);
 
-
-        db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class,"production")
-                .build();
-
-
         button = findViewById(R.id.addFriend);
-
-
-        final AppDatabase db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class,"production")
-                .build();
+        final AppDatabase db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class,"friends").build();
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-
-
                     final Friend  friend = new Friend("Austine", "0718728894", "googleIo");
                     //save the item before leaving the activity
-
 
                     AsyncTask.execute(new Runnable() {
                         @Override
